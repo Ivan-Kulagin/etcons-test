e4f85b09934e5bf5536577188b719bf6997d27b5
Ivan-Kulagin <ivan.kulagin2001@gmail.com>
2023-04-29 20:23:13 +0300

    added routing tabmenu

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="card">
+    <TabMenu :model="items" />
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
+body {
+  font-family: var(--font-family);
+}
+
+.home {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+
+  margin: 0 auto;
+  width: 500px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,6 +29,8 @@
 
 nav {
   padding: 30px;
+  margin: 0 auto;
+  max-width: 1200px;
 }
 
 nav a {
@@ -28,3 +42,36 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+<script setup>
+import TabMenu from 'primevue/tabmenu';
+import { ref } from "vue";
+
+const items = ref([
+  {
+    label: 'Поле Link',
+    icon: 'pi pi-fw pi-external-link',
+    to: '/'
+  },
+  {
+    label: 'Time Picker',
+    icon: 'pi pi-fw pi-calendar',
+    to: '/picker'
+  },
+  {
+    label: 'Сброс значений в Select',
+    icon: 'pi pi-fw pi-times-circle',
+    to: '/multiselect'
+  },
+  {
+    label: 'Многострочный текст в ячейке',
+    icon: 'pi pi-fw pi-list',
+    to: '/multiline'
+  },
+  {
+    label: 'Таблица с поиском',
+    icon: 'pi pi-fw pi-cog',
+    to: '/datatable'
+  }
+]);
+
+</script>
