58f119345658b4d4048f9dbbba6ba84d246b3e4e
Ivan-Kulagin <ivan.kulagin2001@gmail.com>
2023-04-29 20:25:10 +0300

    add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/InputLinkView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,13 +8,25 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/picker',
+    name: 'picker',
+    component: () => import('../views/TimePickerView.vue')
+  },
+  {
+    path: '/multiselect',
+    name: 'multiselect',
+    component: () => import('../views/MultiSelectView.vue')
+  },
+  {
+    path: '/multiline',
+    name: 'multiline',
+    component: () => import('../views/MultiLineView.vue')
+  },
+  {
+    path: '/datatable',
+    name: 'datatable',
+    component: () => import('../views/DataTableView.vue')
+  },
 ]
 
 const router = createRouter({
